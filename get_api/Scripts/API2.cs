6e06df291a4982c6396da52c32587407b840374b
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2019-02-23 21:58:51 +0800

    Modify script to allow flexible input of food

--- get_api/Scripts/API2.cs
+++ get_api/Scripts/API2.cs
@@ -10,19 +10,25 @@ using SimpleJSON;
 
 public class API2 : MonoBehaviour
 {
-    private const string NUTRITION = "https://nutritionix-api.p.rapidapi.com/v1_1/search/cheddar%20cheese?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat%2Cnf_sugars";
+    private const string NUTRITION = "https://nutritionix-api.p.rapidapi.com/v1_1/search/";
     private const string API_KEY = "badef3930emsh11a0eab9caf9d9ep180fbejsna1fa312cb3f1";
+    //private const string INPUT = "cheddar cheese"; // should be flexible for input
+    public InputField INPUT;
+    private const string FIELDS = "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat";
     public Text responseText;
-    
-public void Request()
+
+    public void Request()
     {
         WWWForm form = new WWWForm();
 
         Dictionary<string, string> headers = form.headers;
         headers["X-RapidAPI-Key"] = API_KEY;
+        string EDITED_INPUT = INPUT.text.Replace(" ", "%20");
+        //Debug.Log(EDITED_INPUT);
+        string CONCAT = NUTRITION + EDITED_INPUT + FIELDS;
+        //Debug.Log(CONCAT);
+        WWW request = new WWW(CONCAT, null, headers);
 
-        WWW request = new WWW(NUTRITION, null, headers);
-        
         StartCoroutine(OnResponse(request));
         //Debug.Log(response)
     }
@@ -32,7 +38,7 @@ public void Request()
         yield return req;
         string my_string = req.text;
         JSONNode data = JSON.Parse(my_string);
-       
+
 
         //converts the number to float!!
         float.TryParse(data["hits"][0]["fields"]["nf_total_fat"].Value, out float something);
