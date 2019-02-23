9edb517a3a90a880eb0cdbbafdea60b702e166fd
Johanna <46072340+johannagwan@users.noreply.github.com>
2019-02-24 02:47:51 +0800

    added carbsTarget n tentative values and output

--- Monmon/Assets/Script/SliderUpdater.cs
+++ Monmon/Assets/Script/SliderUpdater.cs
@@ -5,16 +5,35 @@ using UnityEngine.UI;
 
 public class SliderUpdater : MonoBehaviour
 {
-    public string stat;
+    //public string stat;
     public Slider sl;
     GameManager gm;
+    //public float carbs;
+    public float weight = 500;
+    public string gender;
+
     void awake()
     {
         gm = GameObject.FindObjectOfType<GameManager>();
     }
 
-    void Update ()
+    public float calorieTarget(float weight)
+    { // get calorie in grams
+        return weight * 24;
+    }
+
+    public float carbsTarget()
     {
-       // sl.value = gm.calculateStat(stat)/ gm.getSuggestedStat(stat);
+        return (calorieTarget(weight) / 4) * 0.65f;
+    }
+    void Update()
+    {
+        //100 is a dummy value for the intake value tentatively
+        sl.value = 100 / carbsTarget();
+        // if sl.value > 0.6, then things start happening to pet
+        if (sl.value > 0.6)
+        {
+            Debug.Log("STOP EATING CARBS"); // tentative output
+        }
     }
 }
