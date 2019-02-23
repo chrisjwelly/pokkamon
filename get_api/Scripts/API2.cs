aae7e35ba0340464e2d09673491ce4b8b9b2a009
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2019-02-23 22:27:35 +0800

    Add 'exception handling'

--- get_api/Scripts/API2.cs
+++ get_api/Scripts/API2.cs
@@ -12,7 +12,6 @@ public class API2 : MonoBehaviour
 {
     private const string NUTRITION = "https://nutritionix-api.p.rapidapi.com/v1_1/search/";
     private const string API_KEY = "badef3930emsh11a0eab9caf9d9ep180fbejsna1fa312cb3f1";
-    //private const string INPUT = "cheddar cheese"; // should be flexible for input
     public InputField INPUT;
     private const string FIELDS = "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat";
     public Text responseText;
@@ -24,13 +23,9 @@ public class API2 : MonoBehaviour
         Dictionary<string, string> headers = form.headers;
         headers["X-RapidAPI-Key"] = API_KEY;
         string EDITED_INPUT = INPUT.text.Replace(" ", "%20");
-        //Debug.Log(EDITED_INPUT);
         string CONCAT = NUTRITION + EDITED_INPUT + FIELDS;
-        //Debug.Log(CONCAT);
         WWW request = new WWW(CONCAT, null, headers);
-
         StartCoroutine(OnResponse(request));
-        //Debug.Log(response)
     }
 
     private IEnumerator OnResponse(WWW req)
@@ -43,8 +38,13 @@ public class API2 : MonoBehaviour
         //converts the number to float!!
         float.TryParse(data["hits"][0]["fields"]["nf_total_fat"].Value, out float something);
         Debug.Log(something);
-        responseText.text = (something).ToString("0.00");
-        //data["hits"][0]["fields"]["nf_total_fat"].Value
-
+        if (data["max_score"].Value == "null")
+        {
+            responseText.text = "Invalid data. Please input again";
+        }
+        else
+        {
+            responseText.text = "Total fat consumed is: " + (something).ToString("0.00");
+        }
     }
 }
